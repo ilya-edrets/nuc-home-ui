a1bbeeea1f05ab252dcfe094c99e9ac1e4456dc3
Ilya Edrets <iedrets@intermedia.net>
2022-01-22 01:01:47 +0300

    add urls

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
-const nucServices = [{ name: "Portainer", url: "http://nuc:9000" }];
+const nucServices = [
+    { name: "Transmission", url: "http://nuc:9091" },
+    { name: "Portainer", url: "http://nuc:9000" },
+    { name: "Grafana", url: "http://nuc:3000" },
+];
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
